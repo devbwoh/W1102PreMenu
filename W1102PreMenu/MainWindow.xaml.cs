00599622879da157261da08ef41d1b207a2d4dda
BWOH <bwoh@outlook.kr>
2022-11-22 03:13:39 +0900

    초기 커밋.

---
+++ W1102PreMenu/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace W1102PreMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ItemWhite.IsEnabled = false;
+
+            CommandBinding bind = new CommandBinding(ApplicationCommands.Open);
+            bind.Executed += OpenDocument;
+            CommandBindings.Add(bind);
+        }
+
+        private void OpenDocument(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            //dialog.FileName = "사진"; // Default file name
+            dialog.DefaultExt = ".jpg"; // Default file extension
+            dialog.Filter = "Images (.jpg)|*.jpg"; // Filter files by extension
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result != true)
+                return;
+
+            // Open document
+            string filename = dialog.FileName;
+
+            MessageBox.Show(filename);
+        }
+
+        private void SetRed(object sender, RoutedEventArgs e)
+        {
+            //MenuItem item = (MenuItem)sender;
+            //item.IsChecked = true;
+
+            BackPanel.Background = Brushes.Red;
+
+            ItemRed.IsChecked = true;
+            ItemGreen.IsChecked = false;
+            ItemBlue.IsChecked = false;
+
+            ItemWhite.IsEnabled = true;
+        }
+
+        private void SetGreen(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush brush = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+            BackPanel.Background = brush;
+
+            ItemRed.IsChecked = false;
+            ItemGreen.IsChecked = true;
+            ItemBlue.IsChecked = false;
+
+            ItemWhite.IsEnabled = true;
+        }
+
+        private void SetBlue(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush brush = new SolidColorBrush(Color.FromArgb(255, 0, 0, 255));
+            BackPanel.Background = brush;
+
+            ItemRed.IsChecked = false;
+            ItemGreen.IsChecked = false;
+            ItemBlue.IsChecked = true;
+
+            ItemWhite.IsEnabled = true;
+        }
+
+        private void SetWhite(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush brush = new SolidColorBrush(Colors.White);
+            BackPanel.Background = brush;
+
+            ItemRed.IsChecked = false;
+            ItemGreen.IsChecked = false;
+            ItemBlue.IsChecked = false;
+
+            ItemWhite.IsEnabled = false;
+        }
+    }
+}
